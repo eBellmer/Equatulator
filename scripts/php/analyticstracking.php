ae3a0cc975bed3fa528b3685005b7516936970a1
Ethan Bellmer <e.bellmer@outlook.com>
2017-03-24 18:01:26 +0000

    Corrected analytics layout

--- scripts/php/analyticstracking.php
+++ scripts/php/analyticstracking.php
@@ -1,11 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ebell
- * Date: 24/03/2017
- * Time: 05:50 PM
- */
-
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -16,4 +8,3 @@ ga('create', 'UA-96097077-1', 'auto');
 ga('send', 'pageview');
 
 </script>
-?>
