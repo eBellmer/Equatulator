ce7d10215df0d0b9f9eb565a4a8164bfd6b610cc
Ethan Bellmer <e.bellmer@outlook.com>
2017-03-31 22:20:47 +0100

    Updated about, and fixed some formatting issues

--- about/index.php
+++ about/index.php
@@ -16,68 +16,66 @@ define("ROOT_DIR", "./../")
 		<title>About</title>
   </head>
   <body>
-        <?php include_once(ROOT_DIR . "scripts/php/analyticstracking.php") ?>
-        <?php require_once(ROOT_DIR . 'scripts/php/nav.php');?>
-        <div class="outer">
-            <div class="middle">
-                <div class="inner-1">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <h1>About Equatulator</h1>
-                                <div class="row row-equation">
-                                    <div class="col-md-3"></div>
-                                    <div class="col-md-2">
-                                        <h4><a href="aboutAccounts.php">Accounts</a></h4>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <h4><a href="aboutSecurity.php">Security</a></h4>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <h4><a href="faq.php">FAQ</a></h4>
-                                    </div>
-                                    <div class="col-md-3"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-1"></div>
-                            <div class="col-md-5">
-                                <h4>About</h4>
-                                <p class="aboutText">
-                                    Equatulator is a system designed to make your life easier when dealing
-                                    with those arduous scientific equations. <br>
-                                    I hope you get some use out of it.
-                                </p>
-                            </div>
-                            <div class="col-md-5">
-                                <h4>Special Thanks</h4>
-                                <p class="aboutText">
-                                    -Taylor for the inspiration behind the name.<br>
-                                    -That guy for the nav menu and shit<br>
-                                    -That other guy for the login stuff<br>
-                                </p>
-                            </div>
-                            <div class="col-md-1"></div>
-                        </div>
-                        <div class="row row-heading">
-                            <div class="row-md-12">
-                                <h4>Contact</h4>
-                            </div>
-                        </div>
-                        <div class="row row-subheading">
-                            <div class="col-md-3"></div>
-                            <div class="col-md-3">
-                                <p class="aboutText">Email: <br><a href="mailto:admin@equatulator.me">admin@equatulator.me</a></p>
-                            </div>
-                            <div class="col-md-2">
-                                <p class="aboutText">Twitter: <br> Ok</p>
-                            </div>
-                            <div class="col-md-3"></div>
-                        </div>
-                    </div>
+	  <?php include_once(ROOT_DIR . "scripts/php/analyticstracking.php") ?>
+	  <?php require_once(ROOT_DIR . 'scripts/php/nav.php');?>
+	  <div class="outer">
+	    <div class="middle">
+	      <div class="inner-1">
+	        <div class="container-fluid">
+	          <div class="row">
+              <div class="col-md-12">
+                <h1>About Equatulator</h1>
+                <div class="row row-equation">
+                  <div class="col-md-3"></div>
+                  <div class="col-md-2">
+                    <h4><a href="aboutAccounts.php">Accounts</a></h4>
+                  </div>
+                  <div class="col-md-2">
+                    <h4><a href="aboutSecurity.php">Security</a></h4>
+                  </div>
+                  <div class="col-md-2">
+                    <h4><a href="faq.php">FAQ</a></h4>
+                  </div>
+                  <div class="col-md-3"></div>
                 </div>
-            </div>
-        </div>
+              </div>
+	          </div>
+	          <div class="row">
+              <div class="col-md-1"></div>
+              <div class="col-md-5">
+                <h4>About</h4>
+                <p class="aboutText">
+                  Equatulator is a system designed to make your life easier when dealing
+                  with those arduous equations by solving them for you. <br>
+	                The idea is that Equatulator does all the heavy lifting by only requiring the inputs for the equation. <br>
+	                This system was developed as a HND project, and I hope you can get some use out of it.
+                </p>
+              </div>
+              <div class="col-md-5">
+                <h4>Special Thanks</h4>
+                <p class="aboutText">
+                  - Taylor for the inspiration behind the name. <br>
+	                - <a href="http://bootsnipp.com/maridlcrmn">maridlcrmn</a> for their <a href="http://bootsnipp.com/snippets/featured/fancy-sidebar-navigation">"Fancy Sidebar Navigation"</a>. <br>
+	                - <a href="https://github.com/DeveloperShed">DeveloperShed</a> for their <a href="https://github.com/DeveloperShed/BasicSecureLogin">"Basic Secure Login"</a>. <br>
+                </p>
+              </div>
+              <div class="col-md-1"></div>
+	          </div>
+	          <div class="row row-heading">
+              <div class="row-md-12">
+                <h4>Contact</h4>
+              </div>
+	          </div>
+	          <div class="row row-subheading">
+	            <div class="col-md-3"></div>
+	            <div class="col-md-6">
+	              <p class="">Email: <br><a href="mailto:admin@equatulator.me">admin@equatulator.me</a></p>
+	            </div>
+	            <div class="col-md-3"></div>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
   </body>
 </html>
