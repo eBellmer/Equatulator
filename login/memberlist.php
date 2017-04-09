d50d57756e21531a6eb2de1c7b5d66109ce50b3c
Ethan Bellmer <e.bellmer@outlook.com>
2017-04-09 21:39:50 +0100

    Updated some PHP formatting

--- login/memberlist.php
+++ login/memberlist.php
@@ -1,79 +1,89 @@
-<?php 
+<?php
+	define("ROOT_DIR", "./../");
 
-    // First we execute our common code to connection to the database and start the session 
-    require("common.php"); 
-     
-    // At the top of the page we check to see whether the user is logged in or not 
-    if(empty($_SESSION['user']) || $SESSION['auth_level'] !== "admin")
-    { 
-        // If they are not, we redirect them to the login page. 
-        header("Location: login.php"); 
-         
-        // Remember that this die statement is absolutely critical.  Without it, 
-        // people can view your members-only content without logging in. 
-        exit(); 
-    } 
-     
-    // Everything below this point in the file is secured by the login system 
-     
-    // We can retrieve a list of members from the database using a SELECT query. 
-    // In this case we do not have a WHERE clause because we want to select all 
-    // of the rows from the database table. 
-    $query = "SELECT id, username, email FROM users"; 
-     
-    try 
-    { 
-        // These two statements run the query against your database table. 
-        $stmt = $db->prepare($query); 
-        $stmt->execute(); 
-    } 
-    catch(PDOException $ex) 
-    { 
-        // Note: On a production website, you should not output $ex->getMessage(). 
-        // It may provide an attacker with helpful information about your code.  
-        die("Failed to run query: " . $ex->getMessage()); 
-    } 
-         
-    // Finally, we can retrieve all of the found rows into an array using fetchAll 
-    $rows = $stmt->fetchAll();
-		define("ROOT_DIR", "./../");
-?> 
+  // First we execute our common code to connection to the database and start the session
+  require("common.php");
+
+  // At the top of the page we check to see whether the user is logged in or not
+  if(empty($_SESSION['user']) || $_SESSION['user']['auth_level'] !== "admin")
+  {
+    // If they are not, we redirect them to the login page.
+    header("Location: login.php");
+
+    // Remember that this die statement is absolutely critical.  Without it,
+    // people can view your members-only content without logging in.
+    exit();
+  }
+
+  // Everything below this point in the file is secured by the login system
+
+  // We can retrieve a list of members from the database using a SELECT query.
+  // In this case we do not have a WHERE clause because we want to select all
+  // of the rows from the database table.
+  $query = "SELECT id, username, email, auth_level FROM users";
+
+  try
+  {
+    // These two statements run the query against your database table.
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+  }
+  catch(PDOException $ex)
+  {
+    // Note: On a production website, you should not output $ex->getMessage().
+    // It may provide an attacker with helpful information about your code.
+    die("Failed to run query: " . $ex->getMessage());
+  }
+
+  // Finally, we can retrieve all of the found rows into an array using fetchAll
+  $rows = $stmt->fetchAll();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<?php require_once(ROOT_DIR . 'scripts/php/BSCSS.php');?>
+		<link rel="stylesheet" href="<?= ROOT_DIR;?>scripts/css/style.css">
 
-  <!-- JsQuery -->
-  <script src="https://code.jquery.com/jquery-2.1.3.js"></script>
+		<title>Members | Equatulator</title>
+	</head>
+	<body data-spy="scroll" data-offset="50">
+		<?php include_once(ROOT_DIR . "scripts/php/analyticstracking.php") ?>
+		<?php require_once(ROOT_DIR . 'scripts/php/nav.php');?>
+		<div class="outer">
+			<div class="middle">
+				<div class="inner-1">
+					<div class="container-fluid">
+					</div>
+				</div>
+			</div>
+		</div>
 
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-  <title>Members List</title>
-</head>
-<body>
-  <h1>Members | Equatulator</h1>
-  <table> 
-      <tr> 
-          <th>ID</th> 
-          <th>Username</th> 
-          <th>E-Mail Address</th> 
-      </tr> 
-      <?php foreach($rows as $row): ?> 
-          <tr> 
-              <td><?php echo $row['id']; ?></td> <!-- htmlentities is not needed here because $row['id'] is always an integer --> 
-              <td><?php echo htmlentities($row['username'], ENT_QUOTES, 'UTF-8'); ?></td> 
-              <td><?php echo htmlentities($row['email'], ENT_QUOTES, 'UTF-8'); ?></td> 
-          </tr> 
-      <?php endforeach; ?> 
-  </table> 
-  <a href="./../index.php">Go Back</a><br />
-</body>
+
+	  <h1>Members</h1>
+	  <table>
+		  <tr>
+		    <th>ID</th>
+		    <th>Username</th>
+		    <th>E-Mail Address</th>
+			  <th>Auth Level</th>
+		  </tr>
+		  <?php foreach($rows as $row): ?>
+		    <tr>
+	        <td><?= $row['id']; ?></td> <!-- htmlentities is not needed here because $row['id'] is always an integer -->
+	        <td><?= htmlentities($row['username'], ENT_QUOTES, 'UTF-8'); ?></td>
+	        <td><?= htmlentities($row['email'], ENT_QUOTES, 'UTF-8'); ?></td>
+			    <td><?= htmlentities($row['auth_level'], ENT_QUOTES, 'UTF-8'); ?></td>
+		    </tr>
+		  <?php endforeach; ?>
+	  </table>
+	  <a href="<?= ROOT_DIR?>index.php">Go Back</a><br />
+
+
+	</body>
+</html>
