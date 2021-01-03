1f910fed5f37f3635fcf38ec9b887f9bc9073b82
Ethan Bellmer <e.bellmer@outlook.com>
2021-01-03 13:45:34 +0000

    Update mySQLiCon.php

--- scripts/php/mySQLiCon.php
+++ scripts/php/mySQLiCon.php
@@ -6,10 +6,10 @@
  * Time: 12:43 AM
  */
 
-define("DB_HOST", '192.168.1.50');
-define("DB_USER", 'equSQLUser');
-define("DB_PASSWORD", 'bAd93B#1y9r%9xZvj5gT%*mh7gK9y!');
-define("DB_DATABASE", 'equatulator');
+define("DB_HOST", '');
+define("DB_USER", '');
+define("DB_PASSWORD", '');
+define("DB_DATABASE", '');
 
 $con=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 /* if (mysqli_connect_errno($con))
